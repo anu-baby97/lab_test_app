e967105612aed957f6ee8eeb7d1493eb689239f4
anu-baby97 <anubbey@gmail.com>
2021-01-15 21:23:10 +0530

    (login box added)

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -15,40 +15,142 @@ class _LoginScreenState extends State<LoginScreen> {
     return new Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomPadding: true,
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Image.asset("assets/clinic.png"),
-            ],
-          ),
-          SingleChildScrollView(
-              child: Padding(
-            padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
-            child: Column(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Column(
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Image.asset(
-                      "assets/logo.png",
-                      width: ScreenUtil.getInstance().setWidth(70),
-                      height: ScreenUtil.getInstance().setHeight(110),
-                    ),
-                    Text("MEDICOS",
-                        style: TextStyle(
-                            color: Colors.cyan,
-                            fontFamily: "Poppins-Bold",
-                            fontSize: ScreenUtil.getInstance().setSp(40),
-                            letterSpacing: .6,
-                            fontWeight: FontWeight.bold))
-                  ],
+                Container(
+                  color: Color(0XFFF1F7F8),
+                  padding: EdgeInsets.only(left: 20),
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/logo.png",
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("MEDICOS LAB BOOKING ",
+                          style: TextStyle(
+                              color: Colors.cyan,
+                              fontSize: 18,
+                              fontFamily: "Poppins-Bold",
+                              letterSpacing: .6,
+                              fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  "assets/clinic.png",
+                  //fit: BoxFit.fitWidth,
                 ),
               ],
             ),
-          )),
-        ],
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 290),
+                    Padding(
+                      padding: EdgeInsets.only(left: 28.0, right: 28.0),
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black12,
+                                    offset: Offset(0.0, 15.0),
+                                    blurRadius: 15.0),
+                                BoxShadow(
+                                    color: Colors.black12,
+                                    offset: Offset(0.0, -10.0),
+                                    blurRadius: 10.0),
+                              ]),
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 16.0, right: 16.0, top: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text("Login",
+                                    style: TextStyle(
+                                        fontSize:
+                                            ScreenUtil.getInstance().setSp(65),
+                                        fontFamily: "Poppins-Bold",
+                                        letterSpacing: .6)),
+                                SizedBox(
+                                  height:
+                                      ScreenUtil.getInstance().setHeight(30),
+                                ),
+                                Text("Email",
+                                    style: TextStyle(
+                                        fontFamily: "Poppins-Medium",
+                                        fontSize: ScreenUtil.getInstance()
+                                            .setSp(45))),
+                                TextFormField(
+                                  //autofocus: true,
+                                  keyboardType: TextInputType.emailAddress,
+                                  onChanged: (value) {},
+                                  decoration: InputDecoration(
+                                      hintText: "Enter Email",
+                                      hintStyle: TextStyle(
+                                          color: Colors.grey, fontSize: 15.0)),
+                                ),
+                                SizedBox(
+                                  height:
+                                      ScreenUtil.getInstance().setHeight(30),
+                                ),
+                                Text("Password",
+                                    style: TextStyle(
+                                        fontFamily: "Poppins-Medium",
+                                        fontSize: ScreenUtil.getInstance()
+                                            .setSp(45))),
+                                TextFormField(
+                                  onChanged: (value) {},
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                      hintText: "Enter Password",
+                                      hintStyle: TextStyle(
+                                          color: Colors.grey, fontSize: 15.0)),
+                                ),
+                                SizedBox(
+                                  height:
+                                      ScreenUtil.getInstance().setHeight(35),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    InkWell(
+                                      onTap: () {
+//    Navigator.pushNamedAndRemoveUntil();
+                                      },
+                                      child: Text(
+                                        "Forgot Password?",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontFamily: "Poppins-Medium",
+                                            fontSize: ScreenUtil.getInstance()
+                                                .setSp(40)),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
