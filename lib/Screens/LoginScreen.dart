3e89961baf217c017555a230a8220fa0eb0e3cd1
anu-baby97 <anubbey@gmail.com>
2021-01-15 21:08:35 +0530

    (image heading added screenutil used)

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'LoginScreen';
@@ -9,6 +10,46 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Image.asset("assets/clinic.png"),
+            ],
+          ),
+          SingleChildScrollView(
+              child: Padding(
+            padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/logo.png",
+                      width: ScreenUtil.getInstance().setWidth(70),
+                      height: ScreenUtil.getInstance().setHeight(110),
+                    ),
+                    Text("MEDICOS",
+                        style: TextStyle(
+                            color: Colors.cyan,
+                            fontFamily: "Poppins-Bold",
+                            fontSize: ScreenUtil.getInstance().setSp(40),
+                            letterSpacing: .6,
+                            fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ],
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
